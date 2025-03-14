0e4dd1318cbbc752ddf56acf1493f16bd3ae8047
Nabil Tallal <itstallal5@gmail.com>
2025-03-15 00:57:23 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -39,11 +39,10 @@ const App = () => {
 
       <Routes>
           <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-          <Route path="/signup" element={authUser ? <Navigate to="/" /> : <SignUpPage />} />
-          <Route path="/login" element={authUser ? <Navigate to="/" /> : <LoginPage />} />
-          <Route path="/settings" element={authUser ? <SettingsPage /> : <Navigate to="/login" />} />
+          <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+          <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+          <Route path="/settings" element={<SettingsPage />} />
           <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
-          <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <Toaster />
     </div>
